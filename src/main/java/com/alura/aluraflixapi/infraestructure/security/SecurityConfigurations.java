8913451f047338ea6b9b6e0e51c2c451948f47b5
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2023-02-08 11:14:48 +0100

    Implement JWT Token authentication (#5)
    - Add JWT library
    - Implemented AuthenticationController to return user information
    - Implemented TokenService class to generate Token JWT and validate when received in the headers 
    - Implemented configuration to tell to spring to user our SecurityFilter instead their

--- src/main/java/com/alura/aluraflixapi/infraestructure/security/SecurityConfigurations.java
+++ src/main/java/com/alura/aluraflixapi/infraestructure/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package com.alura.aluraflixapi.infraestructure.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Main Spring Security class configuration In Spring 3.0 the security configuration is done by
@@ -20,13 +23,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+  @Autowired
+  private SecurityFilter securityFilter;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     //disabel cross site request forgery
     return http.csrf().disable()
         //Disable Spring controll and expone all endpoints
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().authorizeHttpRequests()
+        //exclude only enpoint Login
+        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+        //any other requeste has to be authenticated
+        .anyRequest().authenticated()
         .and()
+        //tell to spring to user our filter SecurityFilter.class instead their
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
---
+++ src/main/java/com/alura/aluraflixapi/infraestructure/security/dto/TokenJwtDto.java
@@ -0,0 +1,5 @@
+package com.alura.aluraflixapi.infraestructure.security.dto;
+
+public record TokenJwtDto(String token) {
+
+}
