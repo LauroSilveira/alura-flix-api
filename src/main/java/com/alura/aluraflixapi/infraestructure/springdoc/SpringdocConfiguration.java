074bd5452279f7d2dfe8e9f38af897e794f0307b
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2023-12-14 08:54:37 +0100

    Added application.yml configuration to sort swagger by method and alphabetic order (#73)

--- src/main/java/com/alura/aluraflixapi/infraestructure/springdoc/SpringdocConfiguration.java
+++ src/main/java/com/alura/aluraflixapi/infraestructure/springdoc/SpringdocConfiguration.java
@@ -11,23 +11,23 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringdocConfiguration {
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI()
-        .components(new Components()
-            .addSecuritySchemes("bearer-key",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
-        .info(new Info().title("alura-flix-api")
-            .description("An API Rest about movies and series that implements Get, Post, Put and Delet methods."
-                + " Also implements JWT security with Spring Framework")
-            .contact(new Contact()
-                .name("Lauro Correia")
-                .email("lauro.silveira@ymail.com"))
-            .license(new License()
-                .name("Apache 2.0")
-                .url("http://voll.med/api/licenca")
-            )
-        );
-  }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info().title("alura-flix-api")
+                        .description("An API Rest about movies and series that implements Get, Post, Put and Delet methods."
+                                + " Also implements JWT security with Spring Framework")
+                        .contact(new Contact()
+                                .name("Lauro Correia")
+                                .email("lauro.silveira@ymail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://voll.med/api/licenca")
+                        )
+                );
+    }
 
 }
