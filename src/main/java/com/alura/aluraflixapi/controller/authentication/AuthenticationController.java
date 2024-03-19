127fc98e333db4fc68f8fea3f29c4caf740553ec
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2024-03-19 11:46:26 +0100

    Added comments on AuthenticationController (#108)

--- src/main/java/com/alura/aluraflixapi/controller/authentication/AuthenticationController.java
+++ src/main/java/com/alura/aluraflixapi/controller/authentication/AuthenticationController.java
@@ -19,20 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AuthenticationController {
 
-    private final AuthenticationManager manager;
+    private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
-    public AuthenticationController(AuthenticationManager manager, TokenService tokenService) {
-        this.manager = manager;
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
     }
 
     @PostMapping
     public ResponseEntity<TokenJwtDto> login(@RequestBody @Valid AuthenticationDto dto) {
         log.info("Request to Login user: {}", dto.username());
+        // Object wrapper that contains password and user
         final var authenticationToken = new UsernamePasswordAuthenticationToken(dto.username(),
                 dto.password());
-        final var authentication = this.manager.authenticate(authenticationToken);
+        //authenticationManager compare the password of the request with the password from database.
+        final var authentication = this.authenticationManager.authenticate(authenticationToken);
         final var tokenJWT = tokenService.generateTokenJWT((User) authentication.getPrincipal());
         log.info("Token Generated with Success!");
         return ResponseEntity.ok().body(new TokenJwtDto(tokenJWT));
